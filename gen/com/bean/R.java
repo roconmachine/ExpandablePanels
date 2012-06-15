ab220302c8df4273861a9983aa34db39586a0cbb
Arjun Naik <arjun@notikum.com>
2012-06-15 11:26:27 +0530

    Newer build with bug fixes.
    Not using frame layout to expand. Expanding a view based on its height.
    Pretty simple and straight forward.

--- gen/com/bean/R.java
+++ gen/com/bean/R.java
@@ -9,20 +9,128 @@ package com.bean;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int animationDuration=0x7f010003;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int collapsedHeight=0x7f010002;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static int content=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static int handle=0x7f010000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int arrow_dark=0x7f020000;
+        public static int arrow_dark_inverted=0x7f020001;
+        public static int arrow_light=0x7f020002;
+        public static int arrow_light_inverted=0x7f020003;
+        public static int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int frame=0x7f050001;
-        public static final int parent_layout=0x7f050000;
-        public static final int text=0x7f050002;
+        public static int frame=0x7f050007;
+        public static int indicator=0x7f050002;
+        public static int indicator2=0x7f050005;
+        public static int panel=0x7f050000;
+        public static int panel2=0x7f050003;
+        public static int parent_layout=0x7f050006;
+        public static int text=0x7f050001;
+        public static int text2=0x7f050004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static int main=0x7f030000;
+        public static int main_old=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static int app_name=0x7f040001;
+        public static int big_text=0x7f040002;
+        public static int hello=0x7f040000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a ExpandablePanel.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ExpandablePanel_animationDuration com.bean:animationDuration}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandablePanel_collapsedHeight com.bean:collapsedHeight}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandablePanel_content com.bean:content}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandablePanel_handle com.bean:handle}</code></td><td></td></tr>
+           </table>
+           @see #ExpandablePanel_animationDuration
+           @see #ExpandablePanel_collapsedHeight
+           @see #ExpandablePanel_content
+           @see #ExpandablePanel_handle
+         */
+        public static final int[] ExpandablePanel = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.bean.R.attr#animationDuration}
+          attribute's value can be found in the {@link #ExpandablePanel} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:animationDuration
+        */
+        public static final int ExpandablePanel_animationDuration = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.bean.R.attr#collapsedHeight}
+          attribute's value can be found in the {@link #ExpandablePanel} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:collapsedHeight
+        */
+        public static final int ExpandablePanel_collapsedHeight = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.bean.R.attr#content}
+          attribute's value can be found in the {@link #ExpandablePanel} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:content
+        */
+        public static final int ExpandablePanel_content = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.bean.R.attr#handle}
+          attribute's value can be found in the {@link #ExpandablePanel} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:handle
+        */
+        public static final int ExpandablePanel_handle = 0;
+    };
 }
